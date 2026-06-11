1f804c63aea1e1c1993872318835fa8fd4341290
kentaro <xnovasolution688@gmail.com>
2026-06-11 19:41:16 +0000

    feat: fix contact information

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { DEFAULT_PATH } from "@/lib/chattingRoutes";
 import Index from "./pages/Index";
@@ -64,7 +63,6 @@ const App = () => (
         </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
-    <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
   </QueryClientProvider>
 );
 
